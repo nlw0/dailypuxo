3983b9b7352a6b9997ac6bee0ec8e0e6f860534b
nlw0 <nwerneck@gmail.com>
2014-02-22 21:32:20 -0300

    getting my tweets finally working, and with correct oauth

--- dpcrawler/dpcrawler/tools/twitter_crawler.py
+++ dpcrawler/dpcrawler/tools/twitter_crawler.py
@@ -1,12 +1,12 @@
 import oauth2 as oauth
 import json
 
-CONSUMER_KEY = 'qrXCcf1ScgbJrXJeUn1xYg'
-CONSUMER_SECRET = 'odpHntZmyaoI5VkGlAcYpn8fN6PuxDIAUETlrtxG4'
+CONSUMER_KEY = 'wGmcUuiveRydaKiqrHjsUA'
+CONSUMER_SECRET = 'XZBm2sQ5wIp67BE0eewA6S3yGYF4cpbQkEilPMBNwOI'
 ACCESS_KEY = '13129082-cw9rV2cJD5CjMIA8nVvhGmxHPUgXazsDj7DdPKSxx'
 ACCESS_SECRET = 'P7lombxv8bAcMvTsDOCMW61YxmwwO3QL61Y782Pvd0RnZ'
 
-def oauth_req(url, key, secret, http_method="GET", post_body=None,
+def oauth_req(url, key, secret, http_method="GET", body="",
               http_headers=None):
     consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
     token = oauth.Token(key=key, secret=secret)
@@ -15,17 +15,20 @@ def oauth_req(url, key, secret, http_method="GET", post_body=None,
     resp, content = client.request(
         url,
         method=http_method,
-        body=post_body,
+        body=body,
         headers=http_headers
     )
     return content
 
-
-home_timeline = oauth_req(
-    'https://api.twitter.com/1.1/statuses/home_timeline.json',
+timeline = oauth_req(
+    'https://api.twitter.com/1.1/statuses/user_timeline.json?max_id=435159404271255555',
     ACCESS_KEY,
-    ACCESS_SECRET
+    ACCESS_SECRET,
+    body=""
 )
 
-for tw in json.loads(home_timeline):
-    print tw
+# print timeline
+data = json.loads(timeline)
+
+for tw in data:
+    print tw['id'], tw['text']
